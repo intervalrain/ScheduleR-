2002ae6134ad754cb7de1dde4c659ebd9e1bf987
rainhu <intervalrain@gmail.com>
2025-08-01 17:30:25 +0800

    fix assignee unauthorized

--- src/app/api/team/members/route.ts
+++ src/app/api/team/members/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from "next-auth";
+import { authOptions } from '@/lib/auth';
 import prisma from '@/lib/prisma';
 
 export async function GET() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   if (!session || !session.user || !session.user.email) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
